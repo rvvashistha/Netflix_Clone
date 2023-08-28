9cf7f86c63d3acd5d6dca89ee5c75a857a21d2af
unknown <rohitvashistha171@gmail.com>
2023-08-28 11:13:35 +0530

    first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      {/*Nav Bar */}
+      <Nav></Nav>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1>
+      <h1>TEXT</h1> 
+
+      {/*Bannner */}
+
+      {/*Rows */}
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/Nav.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show,handelShow]= useState(false)
+
+  const transitionNavBar=()=>{  
+      if(window.scrollY > 100){
+        handelShow(true)  
+      }
+      else{
+        handelShow(false)
+      }
+  };
+
+  useEffect(()=>{
+    window.addEventListener("scroll",transitionNavBar)
+    return window.removeEventListener("scroll",transitionNavBar)
+  },[])
+
+  return (
+    <div className={`nav ${show && 'nav_black'}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="Netflix_Log"
+        ></img>
+
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        ></img>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
+ 
